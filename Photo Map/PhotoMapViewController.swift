9f3178b2564ce8074402d1100a04ac1d17e49416
Taylor McLaughlin <taylormclaughlin@dhcp-10-132-168-64.uawifi.arizona.edu>
2018-10-19 15:12:10 -0700

    User stories working

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate, MKMapViewDelegate {
     
     
     
@@ -28,7 +28,7 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        theMapView.delegate = self
         let TucsonRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(32.2226, -110.9747), MKCoordinateSpanMake(0.05, 0.05))
         theMapView.setRegion(TucsonRegion, animated: false)
 
@@ -58,6 +58,22 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
         locationsView.delegate = self
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+        
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = UIImage(named: "camera")
+        
+        return annotationView
+    }
+    
     // Protocol Function
     func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
         //print("Lat: \(latitude), Lng: \(longitude)")
@@ -65,6 +81,7 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
         self.navigationController?.popToViewController(self, animated: true)
         let annotation = MKPointAnnotation()
         annotation.coordinate = locationCoordinate
+        annotation.title = "Picture!"
         theMapView.addAnnotation(annotation)
     }
 
